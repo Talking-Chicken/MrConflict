6ac2f7fa650a521c4e9fd7d5c27c7217f830c3b2
Janet Gilbert <janet@wadjeteyegames.com>
2021-11-10 16:15:02 -0500

    Added person

---
+++ Assets/Scripts/MyAmazingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAmazingScript : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject body;
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        head.transform.position += Vector3.up * speed;
+        body.transform.position += Vector3.up * speed;
+    }
+}
