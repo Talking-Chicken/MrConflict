f2d5fbdf32d89d65eb12a3bd970ce93c4c66b031
Janet Gilbert <janet@wadjeteyegames.com>
2021-11-10 16:20:54 -0500

    Added arms and increased head speed.

--- Assets/Scripts/MyAmazingScript.cs
+++ Assets/Scripts/MyAmazingScript.cs
@@ -6,6 +6,7 @@ public class MyAmazingScript : MonoBehaviour
 {
     public GameObject head;
     public GameObject body;
+    public GameObject arms;
     public float speed;
 
     void Start()
@@ -16,7 +17,8 @@ public class MyAmazingScript : MonoBehaviour
     
     void Update()
     {
-        head.transform.position += Vector3.up * speed;
+        head.transform.position += Vector3.up * speed * 1.2f;
         body.transform.position += Vector3.up * speed;
+        arms.transform.position += Vector3.up * speed;
     }
 }
